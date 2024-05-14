bd67c57fe3bed3ce1c7d730c8c1381fb43255220
Ammar Ahmed <ammar69aa59@gmail.com>
2024-05-14 03:06:23 -0700

    rotation added

--- app.js
+++ app.js
@@ -5,42 +5,46 @@ function change1() {
     let box = document.getElementById('box');
     box.style.backgroundColor = '#5ca1e1';
     box.style.transition = 'all 0.9s ease'
-    box.style.scale = '1.2';
+    box.style.scale = '0.6';
     box.style.textAlign = 'center'; // Centering the text
-    box.innerText = 'Hello I\'m Blue !';
+    box.innerText = '';
     box.style.color = 'white';
     box.style.fontSize = '33px';
-    
-    
-    
+    box.style.rotate = "90deg"
+    box.style.boxShadow='4px 4px 10px purple'
+
+
 }
 function change2() {
     let box = document.getElementById('box');
     box.style.backgroundColor = 'black'
     box.style.transition = 'all 0.9s ease'
     box.style.scale = '0.9';
-    box.style.textAlign = 'center'; // Centering the text
-    box.innerText = 'Hello I\'m Black !';
+    box.style.textAlign = 'center'; 
+    box.innerText = '';
     box.style.color = 'white';
     box.style.fontSize = '29px';
+    box.style.rotate = '180deg'
 }
 function change3() {
     let box = document.getElementById('box');
     box.style.backgroundColor = 'red'
     box.style.transition = 'all 0.9s ease'
     box.style.scale = '0.5';
-    box.style.textAlign = 'center'; // Centering the text
-    box.innerText = 'Hello I\'m Red !';
+    box.style.rotate = '360deg'
+    box.style.textAlign = 'center'; 
+    box.innerText = '';
     box.style.color = 'white';
     box.style.fontSize = '50px';
 }
-function change4(){
+function change4() {
     let box = document.getElementById('box');
     box.style.backgroundColor = 'white'
-    box.style.transition = 'all 0.4s ease'
+    box.style.transition = 'all 0.9s ease'
     // box.style.scale = '0.7';
-    box.style.textAlign = 'center'; // Centering the text
+    box.style.textAlign = 'center';
     box.innerText = 'Add color !!';
     box.style.color = 'black';
     box.style.fontSize = '48px';
+    box.style.rotate = '0deg'
 }
